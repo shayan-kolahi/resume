a2235831813756f2882d3644a589b9588c6a39f1
shayan kolahi <shayankolahi30@gmail.com>
2023-03-25 15:01:17 +0430

    make variable for items about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -12,4 +12,41 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  items_aboutMe:any= [
+    {
+      img : "cake-candles",
+      title : "متولد 82 ام",
+    },
+    {
+      img : "location-pin",
+      title : "ساکن تهران",
+    },
+    {
+      img : "graduation-cap",
+      title : "دیپلمه مکانیک ( مایل به ادامه تحصیل در دانشگاه )",
+    },
+  ]
+  items_contactMe:any= [
+    {
+      img : "location-pin",
+      title : "09025354927",
+      type: "tel",
+    },
+    {
+      img : "graduation-cap",
+      title : "shayankolahi30@gmail.com",
+      type:"mailto",
+    },
+  ]
+
+
+  items_socialMedia:any= [
+    "github",
+    "instagram",
+    "telegram",
+    "whatsApp",
+    "discord",
+    "linkedin",
+  ]
+
 }
