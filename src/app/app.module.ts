9c760979b955e1a1921b41f1eb385e9224570eeb
shayan kolahi <shayankolahi30@gmail.com>
2023-03-29 12:42:26 +0430

    again :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,6 @@ import {AppComponent} from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BtnDirective } from './directives/btn.directive';
 import { BannerComponent } from './components/banner/banner.component';
-import { AboutComponent } from './components/about/about.component';
-import { RecentWorksComponent } from './components/recent-works/recent-works.component';
-import { RecentWorkAngularComponent } from './components/recent-work-angular/recent-work-angular.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,10 +12,6 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     BtnDirective,
     BannerComponent,
-    AboutComponent,
-    RecentWorksComponent,
-    RecentWorkAngularComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule
