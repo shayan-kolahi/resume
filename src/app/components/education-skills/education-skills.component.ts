f4629f7de4d56c555953c81b0b79168810ca238a
shayan <shayankolahi30@gmail.com>
2023-08-28 22:45:59 -0700

    make education skills

--- src/app/components/education-skills/education-skills.component.ts
+++ src/app/components/education-skills/education-skills.component.ts
@@ -12,4 +12,39 @@ export class EducationSkillsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  skill:any[] = [
+    {
+      name: "html css",
+      percent: 95
+    },
+    {
+      name: "scss",
+      percent: 75
+    },
+    {
+      name: "bootstrap",
+      percent: 80
+    },
+    {
+      name: "tailwind css",
+      percent: 65
+    },
+    {
+      name: "js",
+      percent: 70
+    },
+    {
+      name: "jquery",
+      percent: 80
+    },
+    {
+      name: "typescript",
+      percent: 60
+    },
+    {
+      name: "angular",
+      percent: 65
+    },
+  ]
+
 }
