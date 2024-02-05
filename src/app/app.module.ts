dff9dccceacd0c999a4ee8b10be24e2bf9f63e50
shayan <shayankolahi30@gmail.com>
2024-02-05 23:09:53 +0330

    favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabViewModule } from 'primeng/tabview';
 import { EducationSkillsComponent } from './components/education-skills/education-skills.component';
 import {NgOptimizedImage} from "@angular/common";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -24,6 +25,12 @@ import {NgOptimizedImage} from "@angular/common";
     BrowserAnimationsModule,
     TabViewModule,
     NgOptimizedImage,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
