da14d5d85e771684ff10a2d6e4342d8e6d40cf9e
shayan <shayankolahi30@gmail.com>
2024-03-27 14:54:47 +0330

    redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,83 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  workSamplesAngular:any[] = [
+    {
+      title: "ادمین پنل",
+      description: "",
+      link: "https://admin-panel.shayankolahi.ir/dashboard",
+    },
+    {
+      title: "فروشگاه",
+      description: "",
+      link: "https://store.shayankolahi.ir/",
+    },
+    {
+      title: "جی مدرن",
+      description: "",
+      link: "https://gmodern.shayankolahi.ir/",
+    },
+  ];
+  workSamplesHtmlCss:any[] = [
+    {
+      img: 12,
+      title: "جهانگردان",
+      link: "https://www.iran-tech.com/under_construction/jahangardan/jahangardan1/home.html",
+    },
+    {
+      img: 2,
+      title: "رکسان گشت",
+      link: "https://www.iran-tech.com/under_construction/roxan/roxan1/home.html",
+    },
+    {
+      img: 3,
+      title: "فلای بوشهر",
+      link: "https://www.iran-tech.com/under_construction/midari/midari_1/home.html",
+    },
+    {
+      img: 6,
+      title: "تهران گشت",
+      link: "https://www.iran-tech.com/under_construction/tehran_gasht/tehran_gasht/home.html",
+    },
+    {
+      img: 7,
+      title: "قصر آسمان",
+      link: "https://www.iran-tech.com/under_construction/ghasre_aseman/ghasre_aseman_4/home.html",
+    },
+    {
+      img: 8,
+      title: "تراولتا سلامت",
+      link: "https://www.iran-tech.com/under_construction/Travelta_Health/Travelta_4/home.html",
+    },
+    {
+      img: 9,
+      title: "تراولتا",
+      link: "https://www.iran-tech.com/under_construction/Travelta/Travelta_7/home.html",
+    },
+    {
+      img: 5,
+      title: "دلنیا گشت",
+      link: "https://www.iran-tech.com/under_construction/dilan_gasht/dilan_gasht_1/home.html",
+    },
+    {
+      img: 10,
+      title: "نیایش سیر",
+      link: "https://www.iran-tech.com/under_construction/niyayesh_seir/niyayesh_seir_2/home.html",
+    },
+    {
+      img: 11,
+      title: "ابر و موج",
+      link: "https://www.iran-tech.com/under_construction/abr_o_moj/abr_o_moj_2/home.html",
+    },
+    {
+      img: 1,
+      title: "جزیره گنج",
+      link: "https://www.iran-tech.com/under_construction/afsaneh_jazireh_ganj/afsaneh_jazireh_ganj1/home.html",
+    },
+    {
+      img: 4,
+      title: "منشور صلح پارسیان",
+      link: "https://www.iran-tech.com/under_construction/manshor/manshor3/home.html",
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import { SidbarComponent } from './components/sidbar/sidbar.component';
-import { AboutComponent } from './components/about/about.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabViewModule } from 'primeng/tabview';
-import { EducationSkillsComponent } from './components/education-skills/education-skills.component';
 import {NgOptimizedImage} from "@angular/common";
 import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -15,10 +11,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 @NgModule({
   declarations: [
     AppComponent,
-    SidbarComponent,
-    AboutComponent,
-    PortfolioComponent,
-    EducationSkillsComponent,
   ],
   imports: [
     BrowserModule,
